3ad901dbd518bd3d0c0290cf42d668a22d2f4233
Miftahul Arifin <miftahul97@gmail.com>
2023-04-02 09:40:41 +0700

    style: add chakra ui lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
---
+++ src/pages/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+
+}
+
+const theme = extendTheme({ colors })
+
+
+export default theme
