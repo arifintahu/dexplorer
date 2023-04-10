adfdd6790b5fc4ce5adff7d3360891afd82907f1
Miftahul Arifin <miftahul97@gmail.com>
2023-04-10 08:59:55 +0700

    feat: add tab blocks

--- src/pages/blocks.tsx
+++ src/pages/blocks.tsx
@@ -18,19 +18,24 @@ import {
   Th,
   Thead,
   Tr,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import { FiChevronRight, FiHome } from 'react-icons/fi'
 import { selectNewBlock } from '@/store/streamSlice'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import { toHex } from '@cosmjs/encoding'
+import { toHex, toBase64 } from '@cosmjs/encoding'
 
 const MAX_BLOCKS = 20
 
 export default function Blocks() {
   const newBlock = useSelector(selectNewBlock)
-  const [events, setEvents] = useState<NewBlockEvent[]>([])
+  const [blocks, setBlocks] = useState<NewBlockEvent[]>([])
 
   useEffect(() => {
     if (newBlock) {
@@ -39,15 +44,15 @@ export default function Blocks() {
   }, [newBlock])
 
   const updateEvent = (event: NewBlockEvent) => {
-    if (events.length) {
-      if (event.header.height > events[0].header.height) {
-        setEvents((prevEvents) => [
+    if (blocks.length) {
+      if (event.header.height > blocks[0].header.height) {
+        setBlocks((prevBlocks) => [
           event,
-          ...prevEvents.slice(0, MAX_BLOCKS - 1),
+          ...prevBlocks.slice(0, MAX_BLOCKS - 1),
         ])
       }
     } else {
-      setEvents([event])
+      setBlocks([event])
     }
   }
 
@@ -86,28 +91,67 @@ export default function Blocks() {
           borderRadius={4}
           p={4}
         >
-          <TableContainer>
-            <Table variant="simple">
-              <Thead>
-                <Tr>
-                  <Th>Height</Th>
-                  <Th>App Hash</Th>
-                  <Th>Txs</Th>
-                  <Th>Time</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {events.map((event) => (
-                  <Tr key={event.header.height}>
-                    <Td>{event.header.height}</Td>
-                    <Td noOfLines={1}>{toHex(event.header.appHash)}</Td>
-                    <Td>{event.txs.length}</Td>
-                    <Td>{timeFromNow(event.header.time.toISOString())}</Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
+          <Tabs variant="unstyled">
+            <TabList>
+              <Tab
+                _selected={{ color: 'white', bg: 'cyan.400' }}
+                borderRadius={5}
+              >
+                Blocks
+              </Tab>
+              <Tab
+                _selected={{ color: 'white', bg: 'cyan.400' }}
+                borderRadius={5}
+              >
+                Transactions
+              </Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <TableContainer>
+                  <Table variant="simple">
+                    <Thead>
+                      <Tr>
+                        <Th>Height</Th>
+                        <Th>App Hash</Th>
+                        <Th>Txs</Th>
+                        <Th>Time</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {blocks.map((block) => (
+                        <Tr key={block.header.height}>
+                          <Td>{block.header.height}</Td>
+                          <Td noOfLines={1}>{toHex(block.header.appHash)}</Td>
+                          <Td>{block.txs.length}</Td>
+                          <Td>
+                            {timeFromNow(block.header.time.toISOString())}
+                          </Td>
+                        </Tr>
+                      ))}
+                    </Tbody>
+                  </Table>
+                </TableContainer>
+              </TabPanel>
+              <TabPanel>
+                <TableContainer>
+                  <Table variant="simple">
+                    <Thead>
+                      <Tr>
+                        <Th>Hash</Th>
+                        <Th>Result</Th>
+                        <Th>Fee</Th>
+                        <Th>Messages</Th>
+                        <Th>Height</Th>
+                        <Th>Time</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody></Tbody>
+                  </Table>
+                </TableContainer>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </Box>
       </main>
     </>
