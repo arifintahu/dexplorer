2df4af5400b65df7e44a338ca329f205d60e4289
Miftahul Arifin <miftahul97@gmail.com>
2023-05-06 09:36:45 +0700

    feat: refactor encoding

---
+++ src/encoding/msg.ts
@@ -0,0 +1,69 @@
+import { MsgSend } from 'cosmjs-types/cosmos/bank/v1beta1/tx'
+import { MsgWithdrawDelegatorReward } from 'cosmjs-types/cosmos/distribution/v1beta1/tx'
+import { MsgDelegate } from 'cosmjs-types/cosmos/staking/v1beta1/tx'
+import { MsgUpdateClient } from 'cosmjs-types/ibc/core/client/v1/tx'
+import { MsgAcknowledgement } from 'cosmjs-types/ibc/core/channel/v1/tx'
+import {
+  MsgExec,
+  MsgGrant,
+  MsgRevoke,
+} from 'cosmjs-types/cosmos/authz/v1beta1/tx'
+import { MsgTransfer } from 'cosmjs-types/ibc/applications/transfer/v1/tx'
+
+const TYPE = {
+  MsgSend: '/cosmos.bank.v1beta1.MsgSend',
+  MsgWithdrawDelegatorReward:
+    '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward',
+  MsgDelegate: '/cosmos.staking.v1beta1.MsgDelegate',
+  MsgUpdateClient: '/ibc.core.client.v1.MsgUpdateClient',
+  MsgAcknowledgement: '/ibc.core.channel.v1.MsgAcknowledgement',
+  MsgExec: '/cosmos.authz.v1beta1.MsgExec',
+  MsgGrant: '/cosmos.authz.v1beta1.MsgGrant',
+  MsgRevoke: '/cosmos.authz.v1beta1.MsgRevoke',
+  MsgTransfer: '/ibc.applications.transfer.v1.MsgTransfer',
+}
+
+export interface DecodeMsg {
+  typeUrl: string
+  data: Object | null
+}
+
+export const decodeMsg = (typeUrl: string, value: Uint8Array): DecodeMsg => {
+  let data = null
+  switch (typeUrl) {
+    case TYPE.MsgSend:
+      data = MsgSend.decode(value)
+      break
+    case TYPE.MsgWithdrawDelegatorReward:
+      data = MsgWithdrawDelegatorReward.decode(value)
+      break
+    case TYPE.MsgDelegate:
+      data = MsgDelegate.decode(value)
+      break
+    case TYPE.MsgUpdateClient:
+      data = MsgUpdateClient.decode(value)
+      break
+    case TYPE.MsgAcknowledgement:
+      data = MsgAcknowledgement.decode(value)
+      break
+    case TYPE.MsgExec:
+      data = MsgExec.decode(value)
+      break
+    case TYPE.MsgGrant:
+      data = MsgGrant.decode(value)
+      break
+    case TYPE.MsgRevoke:
+      data = MsgRevoke.decode(value)
+      break
+    case TYPE.MsgTransfer:
+      data = MsgTransfer.decode(value)
+      break
+    default:
+      break
+  }
+
+  return {
+    typeUrl,
+    data,
+  }
+}
