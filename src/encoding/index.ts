31350a9fd5a50b6295d484dc0f49f33b191e4e50
Miftahul Arifin <miftahul97@gmail.com>
2023-06-16 07:54:22 +0700

    style: fix

--- src/encoding/index.ts
+++ src/encoding/index.ts
@@ -1,4 +1,4 @@
 export type { DecodeMsg } from './msg'
-export { decodeMsg} from './msg'
+export { decodeMsg } from './msg'
 export type { DecodeContentProposal } from './proposal'
-export { decodeContentProposal} from './proposal'
+export { decodeContentProposal } from './proposal'
