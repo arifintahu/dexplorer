f2513efe877d92707d5afc0e18bab1e32ce611b6
Arunpandiaraja <arun@blocktheory.com>
2024-11-08 11:41:34 +0530

    timeline ui update

--- src/components/RecentBlocks/index.tsx
+++ src/components/RecentBlocks/index.tsx
@@ -21,6 +21,30 @@ export default function RecentBlocks() {
 }
 
 const RecentBlock = () => {
+  const dummyData = [
+    {
+      blockId: '291203',
+      address:
+        '0x00000000000000000001e4add93ab2a51d8d405d60177fd30f791027deefaffd',
+      transactions: '1txn',
+      timeAgo: '2hr ago',
+    },
+    {
+      blockId: '291204',
+      address:
+        '0x00000000000000000002e4add93ab2a51d8d405d60177fd30f791027deefb001',
+      transactions: '2txn',
+      timeAgo: '3hr ago',
+    },
+    {
+      blockId: '291204',
+      address:
+        '0x00000000000000000002e4add93ab2a51d8d405d60177fd30f791027deefb001',
+      transactions: '2txn',
+      timeAgo: '3hr ago',
+    },
+    // Add as many data objects as you want here
+  ]
   return (
     <Box bg={'gray-1100'} borderRadius={12}>
       <Box px={4} py={5} bg="gray-1200" borderTopRadius={12}>
@@ -54,7 +78,7 @@ const RecentBlock = () => {
                 '0x00000000000000000001e4add93ab2a51d8d405d60177fd30f791027deefaffd'
               )}
             </Link>
-            <Box w={1} h={1} bg={'gray-500'} borderRadius={99} />
+            <Box w={'2px'} h={'2px'} bg={'gray-500'} borderRadius={99} />
             <Text fontSize={'xs'} color={'gray-400'}>
               2hr ago
             </Text>
@@ -62,7 +86,7 @@ const RecentBlock = () => {
         </HStack>
       </Box>
       <Box px={4} py={'14px'}>
-        <Box className="test" position="relative">
+        <Box position="relative">
           <Box
             position="absolute"
             left="20px"
@@ -73,132 +97,91 @@ const RecentBlock = () => {
             zIndex="1"
           />
 
-          <HStack
-            justifyContent="space-between"
-            className="test"
-            py={'14px'}
-            borderBottom={'1px'}
-            borderColor={'gray-900'}
-            ml={10}
-            position="relative"
-          >
-            <Box
-              position="absolute"
-              left="-21px"
-              top="50%"
-              transform="translateY(-50%)"
-              width="6px"
-              height="6px"
-              borderRadius="full"
-              bg="primary-500"
-              zIndex="2"
-            />
+          {dummyData.map((data, index) => (
+            <HStack
+              key={index}
+              justifyContent="space-between"
+              py="14px"
+              borderBottom="1px"
+              borderColor="gray-900"
+              ml={10}
+              position="relative"
+              _last={{ borderBottom: 'none' }}
+            >
+              <Image
+                src={images.logoShort.src}
+                alt="Surge"
+                position="absolute"
+                left="-30px"
+                top="50%"
+                transform="translateY(-50%)"
+                zIndex={2}
+              />
+              <Box
+                className="dots"
+                position="absolute"
+                left="-22px"
+                top="50%"
+                transform="translateY(-50%)"
+                width="6px"
+                height="6px"
+                borderRadius="full"
+                bg="primary-500"
+                zIndex="2"
+              />
 
-            <HStack gap={3}>
+              <HStack gap={3}>
+                <HStack gap={1}>
+                  <Image width={5} src={images.block.src} alt="block" />
+                  <Link
+                    as={NextLink}
+                    href={`/blocks/${data.blockId}`}
+                    _focus={{ boxShadow: 'none' }}
+                    display="block"
+                    fontSize="14px"
+                    color="primary-200"
+                  >
+                    {data.blockId}
+                  </Link>
+                </HStack>
+              </HStack>
               <HStack gap={1}>
-                <Image width={5} src={images.block.src} alt="block" />
                 <Link
                   as={NextLink}
-                  href={'/blocks'}
+                  href={`/address/${data.address}`}
                   _focus={{ boxShadow: 'none' }}
-                  display={'block'}
-                  fontSize={'14px'}
-                  color={'primary-200'}
+                  fontSize="xs"
+                  color="gray-500"
+                  textDecoration="underline"
                 >
-                  291203
+                  {shortenAddress(data.address)}
                 </Link>
+                <Box w="2px" h="2px" bg="gray-500" borderRadius={99} />
+                <Text fontSize="xs" color="gray-500">
+                  {data.transactions}
+                </Text>
+                <Box w="2px" h="2px" bg="gray-500" borderRadius={99} />
+                <Text fontSize="xs" color="gray-400">
+                  {data.timeAgo}
+                </Text>
               </HStack>
             </HStack>
-            <HStack gap={1}>
-              <Link
-                as={NextLink}
-                href={'/blocks'}
-                _focus={{ boxShadow: 'none' }}
-                fontSize={'xs'}
-                color={'gray-500'}
-                textDecoration={'underline'}
-              >
-                {shortenAddress(
-                  '0x00000000000000000001e4add93ab2a51d8d405d60177fd30f791027deefaffd'
-                )}
-              </Link>
-              <Box w={1} h={1} bg={'gray-500'} borderRadius={99} />
-              <Text fontSize={'xs'} color={'gray-400'}>
-                2hr ago
-              </Text>
-            </HStack>
-          </HStack>
-
-          <HStack
-            justifyContent="space-between"
-            className="test"
-            py={'14px'}
-            borderBottom={'1px'}
-            borderColor={'gray-900'}
-            ml={10}
-            position="relative"
-          >
-            <Box
-              position="absolute"
-              left="-21px"
-              top="50%"
-              transform="translateY(-50%)"
-              width="6px"
-              height="6px"
-              borderRadius="full"
-              bg="primary-500"
-              zIndex="2"
-            />
-
-            <HStack gap={3}>
-              <HStack gap={1}>
-                <Image width={5} src={images.block.src} alt="block" />
-                <Link
-                  as={NextLink}
-                  href={'/blocks'}
-                  _focus={{ boxShadow: 'none' }}
-                  display={'block'}
-                  fontSize={'14px'}
-                  color={'primary-200'}
-                >
-                  291203
-                </Link>
-              </HStack>
-            </HStack>
-            <HStack gap={1}>
-              <Link
-                as={NextLink}
-                href={'/blocks'}
-                _focus={{ boxShadow: 'none' }}
-                fontSize={'xs'}
-                color={'gray-500'}
-                textDecoration={'underline'}
-              >
-                {shortenAddress(
-                  '0x00000000000000000001e4add93ab2a51d8d405d60177fd30f791027deefaffd'
-                )}
-              </Link>
-              <Box w={1} h={1} bg={'gray-500'} borderRadius={99} />
-              <Text fontSize={'xs'} color={'gray-400'}>
-                2hr ago
-              </Text>
-            </HStack>
-          </HStack>
+          ))}
         </Box>
       </Box>
       <Box px={4} py={5} bg="gray-1200" borderBottomRadius={12}>
         <HStack gap={2}>
           <Image w={4} src={images.stars.src} alt="" />
           <HStack>
-            <Text fontSize={'xs'} color={'gray-400'}>
+            <Text fontSize={'xs'} color={'gray-500'}>
               120 txns
             </Text>
-            <Box w={1} h={1} bg={'gray-500'} borderRadius={99} />
-            <Text fontSize={'xs'} color={'gray-400'}>
+            <Box w={'2px'} h={'2px'} bg={'gray-500'} borderRadius={99} />
+            <Text fontSize={'xs'} color={'gray-500'}>
               12 blocks
             </Text>
-            <Box w={1} h={1} bg={'gray-500'} borderRadius={99} />
-            <Text fontSize={'xs'} color={'gray-400'}>
+            <Box w={'2px'} h={'2px'} bg={'gray-500'} borderRadius={99} />
+            <Text fontSize={'xs'} color={'gray-500'}>
               ~6s settlement
             </Text>
           </HStack>
